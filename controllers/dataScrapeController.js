6805f534989d5fc929d89753df3fb81d93e2448f
Sean Ruhland <sean.ruhland@gmail.com>
2018-03-21 14:33:31 -0700

    data get method

---
+++ controllers/dataScrapeController.js
@@ -0,0 +1,7 @@
+exports.getData = function(req,res){
+		
+		//respond with data
+			res.json({message:'there will be data'})
+		
+	
+}
---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+var mongoose = require('mongoose')
+
+//connect to database
+mongoose.connect('mongodb://localhost:27017/blog')
+ 
+mongoose.connection.on('open', function() {console.log('db connected')})
+
+var dataScrapeController = require('./controllers/dataScrapeController')
+
+//read	
+app.get('/data', dataScrapeController.getData)
+
+app.listen(8080)
+console.log('server is running')
